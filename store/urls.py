216d8c8ab8753bb3a2ce0eec043e728ef0a006ac
Sifa Nur Febriani <sifanurfebriani@gmail.com>
2024-04-30 11:21:32 +0700

    pert II fix

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-
 from . import views
+from django.urls import path
+from django.contrib import admin
+
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
         #Leave as empty string for base url
@@ -9,3 +13,5 @@ urlpatterns = [
 	path('checkout/', views.checkout, name="checkout"),
 
 ]
+
+urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
